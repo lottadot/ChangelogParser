d275ae0464752b9205a0355c6c797a2c5a69db09
Shane Zatezalo <shane@lottadot.com>
2016-08-21 09:14:42 -0400

    Add initial Xcode workspace and project

---
+++ changelogparser/changelogparser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  changelogparser
+//
+//  Created by Shane Zatezalo on 8/21/16.
+//  Copyright © 2016 Lottadot LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
